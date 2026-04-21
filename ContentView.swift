d85b6933a3514e0d9790e0f8e2547d1ffbce1ad8
OskarF <oskarfab4353@gmail.com>
2026-04-21 10:40:48 -0500

    i added a basketball button with a fixed size

--- ContentView.swift
+++ ContentView.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      Text("Welocme")
+      Text("Welcome")
             .font(.largeTitle)
         Text("Select Your Sport")
         Text("(click the icon)")
+        
+        Button("🏀") {
+            
+        }
+        .font(.custom("basketball", fixedSize: 125))
     }
 }
