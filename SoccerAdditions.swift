66edb64a40131285f1b8e67206ce76a0a2f916ac
AndrewZ <p075908912@gmail.com>
2026-04-23 10:43:20 -0500

    added navigation links and new page

---
+++ SoccerAdditions.swift
@@ -0,0 +1,14 @@
+//
+//  SoccerAdditions.swift
+//  Sports
+//
+//  Created by Andrew S. Zhundrikov on 4/23/26.
+//
+
+import SwiftUI
+
+struct Soccermore: View {
+    var body: some View {
+        
+    }
+}
