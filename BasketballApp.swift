7ae7934eea6cd3c7528489393257bf459dbf7e7a
OskarF <oskarfab4353@gmail.com>
2026-04-23 11:10:06 -0500

    i added navigation links to seperate pages to make a play and look at your plays

--- BasketballApp.swift
+++ BasketballApp.swift
@@ -9,6 +9,14 @@ import SwiftUI
 struct PlayMaker: View {
     var body: some View {
         Text("Welcome To Playmaker!!")
+        
+        NavigationLink("Go to your Playlist") {
+            PlayList()
+        }
+        
+        NavigationLink("Go Make A Play") {
+            PlayMaker()
+        }
     }
 }
 
