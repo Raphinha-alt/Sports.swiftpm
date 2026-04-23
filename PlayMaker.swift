34a554ec78da4484e8b48280573e29bda227ba28
OskarF <oskarfab4353@gmail.com>
2026-04-23 11:06:51 -0500

    i added a file with a play list and a file for a play maker

---
+++ PlayMaker.swift
@@ -0,0 +1,13 @@
+//
+//  PlayMaker.swift
+//  Sports
+//
+//  Created by Oskar Fabisiak on 4/23/26.
+//
+import SwiftUI
+
+struct MakeAPlay: View {
+    var body: some View {
+        Text("Make a play")
+    }
+}
