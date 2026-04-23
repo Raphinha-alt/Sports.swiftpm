d480737aad5fc146c6284ba00e6182e5c505471c
OskarF <oskarfab4353@gmail.com>
2026-04-23 11:14:49 -0500

    i made the main page look more neat with modifiers

--- BasketballApp.swift
+++ BasketballApp.swift
@@ -8,15 +8,34 @@ import SwiftUI
 
 struct PlayMaker: View {
     var body: some View {
-        Text("Welcome To Playmaker!!")
-        
-        NavigationLink("Go to your Playlist") {
-            PlayList()
-        }
-        
-        NavigationLink("Go Make A Play") {
-            PlayMaker()
+        NavigationStack {
+            VStack(spacing: 30) {
+                
+                Text("Welcome to PlayMaker")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                
+                NavigationLink(destination: PlayList()) {
+                    Text("Go to Your Playlist")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                }
+                
+                NavigationLink(destination: MakeAPlay()) {
+                    Text("Go Make a Play")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                }
+                
+                Spacer()
+            }
         }
     }
 }
-
