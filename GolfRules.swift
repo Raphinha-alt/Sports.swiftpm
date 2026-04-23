68944b6761b8e37957328d4e44f70054f4df5aa5
Oleksandr <oandrusyshyn7497@stu.d214.org>
2026-04-23 10:22:14 -0500

    Progress on the golf rules text

--- GolfRules.swift
+++ GolfRules.swift
@@ -8,6 +8,34 @@ import SwiftUI
 
 struct GolfRules: View {
     var body: some View {
-        Text("golf rules here")
+        Text("Golf Rules")
+        VStack(alignment: .leading){
+            
+            Text("Main rules:")
+            Text("""
+             Start in within the tee box, between the designated markers and no longer than too clubs behind.
+             Play the ball as it lies.
+             In penalty areas you can play as it lies or take a relief and get a one-stroke penalty.
+             You can lift the ball to clean it but it must be put on the exact same place.
+             If you cannot find your ball in 3 minutes or it went out of bounds, you must replay the shot from the previous spot taking a one-stroke penalty.
+             You can carry no more than 14 clubs.
+             """)
+            //        Penalties:
+            //        One Stroke: Penalty areas, lost ball, taking unplayable lie, or moving your ball during practice swing.
+            //        Two strokes: Given for playing from the wrong place, playing wrong ball, or violating rule in a bunker.
+            //                                Disqualification: Given for submitting lower score or consistent breaches of etiquette.
+            //                                Etiquette:
+            //                                    Shout “Fore!” If your ball heading toward other golfers.
+            //                                Do not disturb others when they prepare to hit.
+            //                                Always be ready to play and keep up with the group in front.
+            //                                Repair divots, smooth bunker sand, and fix ball marks.
+            //                                Clubs:
+            //                                    Driver: Used for maximum distance off the tee.
+            //                                Fairway woods: For long shots from the fairway or rough.
+            //                                Hybrids: Replacement for long irons with better control and easier launch.
+            //                                Irons: Used to approach shots to the green.
+            //                                Wedges: Specialized clubs for short-game shots, including pitching, gap, sand, and lob wedges.
+            //                                Putter: Short range hits on green to roll the ball into the hole.
+        }
     }
 }
