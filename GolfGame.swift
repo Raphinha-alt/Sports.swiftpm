d1e11a809352d16d6082f50554be7f0c29a633d9
Oleksandr <oandrusyshyn7497@stu.d214.org>
2026-04-23 10:44:15 -0500

    Added a golf game page

---
+++ GolfGame.swift
@@ -0,0 +1,13 @@
+//
+//  GolfGame.swift
+//  Sports
+//
+//  Created by Oleksandr Andrusyshyn on 4/23/26.
+//
+import SwiftUI
+
+struct GolfGame: View {
+    var body: some View {
+        Text("Golf")
+    }
+}
