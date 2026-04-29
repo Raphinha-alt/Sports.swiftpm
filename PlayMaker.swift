2b02f5dbdaf529469b515a02623a3edec11e8053
OskarF <oskarfab4353@gmail.com>
2026-04-29 10:55:28 -0500

    i added a page where making the play is possible but so far without movement

--- PlayMaker.swift
+++ PlayMaker.swift
@@ -5,66 +5,71 @@
 //  Created by Oskar Fabisiak on 4/23/26.
 //
 import SpriteKit
-
 class BasketballGameScene: SKScene {
-
+    let offenseColor = SKColor.blue
+    let defenseColor = SKColor.red
+    let ballColor = SKColor.orange
+    var players: [SKShapeNode] = []
+    var defenders: [SKShapeNode] = []
+    var ball = SKShapeNode(circleOfRadius: 12)
     override func didMove(to view: SKView) {
-
-        backgroundColor = .darkGray
-
-        print("🔥 Scene is running")
-
+        backgroundColor =
+            .white
         drawCourt()
-        spawnPlayers()
+        createPlayers()
+        createDefense()
+        createBall()
     }
-
-    // MARK: COURT (VERY SIMPLE, ALWAYS VISIBLE)
     func drawCourt() {
-
-        let court = SKShapeNode(rectOf: CGSize(width: 300, height: 500))
-        court.fillColor = .orange
-        court.strokeColor = .white
+        let court = SKShapeNode(rectOf: CGSize(width: 350, height: 500))
+        court.strokeColor =
+            .black
         court.lineWidth = 4
         court.position = CGPoint(x: frame.midX, y: frame.midY)
         addChild(court)
-
-        let hoop = SKShapeNode(circleOfRadius: 10)
-        hoop.fillColor = .red
-        hoop.position = CGPoint(x: frame.midX, y: frame.midY - 200)
+        let hoop = SKShapeNode(circleOfRadius: 15)
+        hoop.strokeColor =
+            .orange
+        hoop.lineWidth = 4
+        hoop.position = CGPoint(x: frame.midX, y: frame.maxY - 100)
         addChild(hoop)
     }
-
-    // MARK: PLAYERS (STATIC BUT GUARANTEED VISIBLE)
-    func spawnPlayers() {
-
-        for i in 0..<5 {
-
-            let offense = SKShapeNode(circleOfRadius: 12)
-            offense.fillColor = .blue
-            offense.position = CGPoint(
-                x: frame.midX + CGFloat(i - 2) * 40,
-                y: frame.midY + 120
-            )
-            addChild(offense)
-
-            let defense = SKShapeNode(circleOfRadius: 12)
-            defense.fillColor = .red
-            defense.position = CGPoint(
-                x: frame.midX + CGFloat(i - 2) * 40,
-                y: frame.midY + 40
-            )
-            addChild(defense)
+    func createPlayers() {
+        let positions = [
+            CGPoint(x: frame.midX - 100, y: 250),
+            CGPoint(x: frame.midX + 100, y: 250),
+            CGPoint(x: frame.midX, y: 350),
+            CGPoint(x: frame.midX - 80, y: 450),
+            CGPoint(x: frame.midX + 80, y: 450)
+        ]
+        for pos in positions {
+            let player = SKShapeNode(circleOfRadius: 20)
+            player.fillColor = offenseColor
+            player.position = pos
+            player.name = "player"
+            addChild(player)
+            players.append(player)
         }
-
-        let ball = SKShapeNode(circleOfRadius: 6)
-        ball.fillColor = .orange
-        ball.position = CGPoint(x: frame.midX, y: frame.midY + 120)
+    }
+    func createDefense() {
+        let positions = [
+            CGPoint(x: frame.midX - 120, y: 320),
+            CGPoint(x: frame.midX + 120, y: 320),
+            CGPoint(x: frame.midX, y: 420),
+            CGPoint(x: frame.midX - 60, y: 520),
+            CGPoint(x: frame.midX + 60, y: 520)
+        ]
+        for pos in positions {
+            let defender = SKShapeNode(circleOfRadius: 20)
+            defender.fillColor = defenseColor
+            defender.position = pos
+            addChild(defender)
+            defenders.append(defender)
+        }
+    }
+    func createBall() {
+        ball.fillColor = ballColor
+        ball.position = CGPoint(x: frame.midX, y: 350)
         addChild(ball)
-
-        let label = SKLabelNode(text: "WORKING BASKETBALL SCENE")
-        label.fontSize = 20
-        label.fontColor = .white
-        label.position = CGPoint(x: frame.midX, y: frame.midY + 220)
-        addChild(label)
     }
 }
