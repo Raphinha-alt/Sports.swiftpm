0fc1f1191b59fcc9ce2a6598b79b137822f1890f
AndrewZ <p075908912@gmail.com>
2026-05-11 11:14:09 -0500

    added another rivarly

--- soccerrials.swift
+++ soccerrials.swift
@@ -11,8 +11,10 @@ import SwiftUI
 struct Rivals: View {
     @State var showAlert1 = false
     @State var showAlert2 = false
+    @State var showAlert3 = false
     @State var R1 = ["madrid", "sword", "bayern"]
     @State var R2 = ["barca", "sword", "milan"]
+    @State var R3 = ["Chelsea", "sword", "United"]
     var body: some View {
         Text("Biggest Uefa Rivalries")
             .font(.largeTitle)
@@ -34,6 +36,7 @@ struct Rivals: View {
         .alert("Matches played: 30, Madrid wins: 13, Bayern wins: 13, Draws: 4", isPresented: $showAlert1) {
                                     }
             }
+            .padding(35)
             HStack{
                 ForEach(R2.indices, id: \.self) { index in
                     Image(R2[index])
@@ -47,10 +50,27 @@ struct Rivals: View {
         .frame(width: 150, height: 150)
         .background(Color.red)
         .foregroundColor(.blue)
-        .alert("Matches played: 15, Barca wins: 7, Milan wins: 3, Draws: 5", isPresented: $showAlert1) {
+        .alert("Matches played: 15, Barca wins: 7, Milan wins: 3, Draws: 5", isPresented: $showAlert2) {
                                     }
-
             }
+            .padding(35)
+            HStack{
+                ForEach(R3.indices, id: \.self) { index in
+                    Image(R3[index])
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                }
+                Button("Show Stats") {
+                    showAlert3.toggle()
+                }
+        .font(.largeTitle)
+        .frame(width: 150, height: 150)
+        .background(Color.red)
+        .foregroundColor(.blue)
+        .alert("Matches played: 195, Chelsea wins: 56, United wins: 83, Draws: 56", isPresented: $showAlert3) {
+                                    }
+            }
+            
         }
     }
 }
