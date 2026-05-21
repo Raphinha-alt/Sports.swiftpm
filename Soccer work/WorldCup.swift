49076e6bbd1e19b031a3fc0886c4d70db4d71081
AndrewZ <p075908912@gmail.com>
2026-05-21 11:34:25 -0500

    added a new picture, page

---
+++ Soccer work/WorldCup.swift
@@ -0,0 +1,14 @@
+//
+//  WorldCup.swift
+//  Sports
+//
+//  Created by Andrew S. Zhundrikov on 5/21/26.
+//
+
+import SwiftUI
+
+struct WorldCup: View {
+    var body: some View {
+        
+    }
+}
