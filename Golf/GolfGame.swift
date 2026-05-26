989406aa60eb968d2f5c494ebc59076119d606d3
Oleksandr <oandrusyshyn7497@stu.d214.org>
2026-05-26 11:16:56 -0500

    Added text below buttons.

--- Golf/GolfGame.swift
+++ Golf/GolfGame.swift
@@ -18,49 +18,56 @@ struct GolfGame: View {
     let clubs = ["Driver", "Iron", "Putter"]
     
     var body: some View {
-        
         HStack (alignment: .top, spacing: 400){
             Button{
                 dismiss()
             } label: {
-                Text("🏠")
-                    .font(.custom("", size: 60))
+                VStack{
+                    Text("🏠")
+                        .font(.custom("", size: 80))
+                    Text("Home")
+                        .font(.largeTitle)
+                }
             }
             
             Button {
                 restart = UUID()
             } label: {
-                Image(systemName: "repeat")
-                    .font(.custom("", size: 60))
-            }
-        }
-        
-        
-        ZStack {
-            GeometryReader() { geometry in
-                SpriteView(scene: GameScene(powers: $power, clubSpecific: $pickedClub, size: geometry.size))
-            }
-            .id(restart)
-        }
-        .toolbar {
-            
-            ToolbarItem(placement: .bottomBar) {
-                Slider(value: $power, in: 1...10, step: 1.0) {_ in
+                VStack{
+                    Image(systemName: "repeat")
+                        .font(.custom("", size: 80))
+                    Text("Restart")
+                        .font(.largeTitle)
                 }
-                .frame(width: .infinity)
-                .padding()
             }
-            
-            ToolbarItem(placement: .topBarLeading){
-                Picker("Pick your club", selection: $pickedClub){
-                    ForEach(clubs, id: \.self){ club in
-                        Text(club).tag(club)
-                            .font(.custom("My", size: 100))
+        }
+        ZStack{
+            ZStack {
+                GeometryReader() { geometry in
+                    SpriteView(scene: GameScene(powers: $power, clubSpecific: $pickedClub, size: geometry.size))
+                }
+                .id(restart)
+            }
+            .toolbar {
+                
+                ToolbarItem(placement: .bottomBar) {
+                    Slider(value: $power, in: 1...10, step: 1.0) {_ in
+                    }
+                    .frame(width: .infinity)
+                    .padding()
+                }
+                
+                ToolbarItem(placement: .topBarLeading){
+                    Picker("Pick your club", selection: $pickedClub){
+                        ForEach(clubs, id: \.self){ club in
+                            Text(club).tag(club)
+                                .font(.custom("My", size: 100))
+                        }
                     }
                 }
+                
             }
         }
-        
     }
 }
 
